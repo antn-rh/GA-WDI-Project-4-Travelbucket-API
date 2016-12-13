70d8e46020580e4b4165a653a286f9d32d9c03c6
Antonio Rhee <antoniolrhee@gmail.com>
2016-12-13 09:44:12 -0800

    adds flightSchema and embeds it to tripSchema in trip.js model

--- models/trip.js
+++ models/trip.js
@@ -1,14 +1,19 @@
 var mongoose = require('mongoose');
 
+var flightSceham = new mongoose.Schema){
+  flightOrigin: String,
+  flightDestination: String,
+  flightDepartureTime: String,
+  flightDuration: String,
+}
+
 var tripSchema = new mongoose.Schema({
   createdBy: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
   location: String,
   startDate: Date,
   endDate: Date,
-  flightOrigin: String,
-  flightDestination: String,
-  flightDepartureTime: String,
-  flightDuration: String,
+  departingFlight: [flightSchema],
+  returnFlight: [flightSchema],
   lodgingAddress: String,
   bookmarks: []
 });
